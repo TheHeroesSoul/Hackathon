dae04d7cdbb1c7e5d52cf924d747595e0225d198
Giulio Giangrande <g.giangrande@studenti.unina.it>
2025-04-16 15:20:20 +0200

    attributi

--- src/Partecipante.java
+++ src/Partecipante.java
@@ -1,12 +1,13 @@
 public class Partecipante extends Utente {
+    private String nomeTeam;
 
     public Partecipante(String email, String nome, String cognome) {
         super(email, nome, cognome);
+        this.nomeTeam = nomeTeam;
     }
 
     private String invitoTeam() {
-        String messaggioPartecipante= "Abbiamo bisogno di te! Sei dei nostri?";
-        return messaggioPartecipante;
+        return "Abbiamo bisogno di te! Sei dei nostri?";
     }
 
 }
