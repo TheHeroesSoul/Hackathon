83e09b261ac01f8f94180af7596f085bff66b815
Lorenzo Lista <99364774+TheHeroesSoul@users.noreply.github.com>
2025-05-12 16:31:41 +0200

    Creazione Struttura Estetica
    Creata l'estetica della prima parte di pagine

--- src/main/java/gui/Home.java
+++ src/main/java/gui/Home.java
@@ -1,4 +1,11 @@
 package main.java.gui;
 
+import javax.swing.*;
+
 public class Home {
+    private JList list1;
+    private JButton infoUtenteButton;
+    private JButton creaHackathonButton;
+    private JButton logOutButton;
+    private JList list2;
 }
---
+++ src/main/java/gui/LogIn.java
@@ -0,0 +1,10 @@
+package main.java.gui;
+
+import javax.swing.*;
+
+public class LogIn {
+    private JPasswordField passwordField1;
+    private JPanel panel1;
+    private JTextField textField1;
+    private JButton enterButton;
+}
